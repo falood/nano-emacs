cdee51a67f40299e82b3f418e51500c7433c7527
Xiangrong Hao <falood@gmail.com>
2021-05-25 15:42:43 -0700

    Update nano-layout.el

--- nano-layout.el
+++ nano-layout.el
@@ -20,8 +20,7 @@
 
 (setq default-frame-alist
       (append (list
-	       '(font . "Roboto Mono:style=Light:size=14")
-	       ;; '(font . "Roboto Mono Emacs Regular:size=14")
+	       '(font . "Victor Mono:style=Light:size=16")
 	       '(min-height . 1)  '(height     . 45)
 	       '(min-width  . 1) '(width      . 81)
                '(vertical-scroll-bars . nil)
@@ -39,16 +38,6 @@
 ;; defaults write org.gnu.Emacs AppleFontSmoothing -int 1 (light)
 ;; defaults write org.gnu.Emacs AppleFontSmoothing -int 2 (medium)
 ;; defaults write org.gnu.Emacs AppleFontSmoothing -int 3 (strong)
-
-;; Fall back font for glyph missing in Roboto
-(defface fallback '((t :family "Fira Code"
-                       :inherit 'nano-face-faded)) "Fallback")
-(set-display-table-slot standard-display-table 'truncation
-                        (make-glyph-code ?… 'fallback))
-(set-display-table-slot standard-display-table 'wrap
-                         (make-glyph-code ?↩ 'fallback))
-
-;; (set-fontset-font t nil "Fira Code" nil 'append)
 
 ;; Fix bug on OSX in term mode & zsh (spurious % after each command)
 (add-hook 'term-mode-hook
